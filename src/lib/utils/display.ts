5dbb816860e69685a879b74190dc33e94c1593a8
Beastman Kojak <beastman.kojak@gmail.com>
2022-02-22 00:40:46 -0500

    Add a null check to the display helper

--- src/lib/utils/display.ts
+++ src/lib/utils/display.ts
@@ -2,5 +2,8 @@ const display = (
   traits: { attributes: Array<Array<{ label: string; value: string }>> },
   trait: string,
   value: string
-) => traits.attributes[trait].filter(({ value: val }) => val === value)[0].label;
+) => {
+  const attr = traits.attributes[trait].filter(({ value: val }) => val === value)[0];
+  return attr && attr.label;
+};
 export default display;
